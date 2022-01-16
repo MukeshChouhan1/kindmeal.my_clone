d59db754fbb4171b304ec81a894ab17a02bf0cd3
Mukesh Chouhan <95852087+Mukeshfw14586@users.noreply.github.com>
2022-01-16 12:27:16 +0530

    Add files via upload

---
+++ kindmeal/clone/homepage.js
@@ -0,0 +1,15 @@
+function goTo() {
+    window.location.href = "productProfile.html"
+}
+
+function goToLogIn() {
+    window.location.href = "logIn.html"
+}
+
+function goToMealDeals() {
+    window.location.href = "MealDeals.html"
+}
+
+function goToJoin() {
+    window.location.href = "joinKindMeal.html"
+}
